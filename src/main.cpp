1ca914508dfd02d74033e8e9910ee114332c1bf1
bkandh30 <kandhari.bhavya20@gmail.com>
2025-10-30 22:54:25 -0700

    Phase 6: Added configurable ports from CLI args

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
 #include "server.hpp"
+#include "logger.hpp"
+#include <cstdlib>
 
-int main() {
-    Server server(8080);
+int main(int argc, char* argv[]) {
+    int port = 8080;
+    if (argc > 1) port = std::stoi(argv[1]);
+    else if (const char* env_port = std::getenv("PORT")) port = std::stoi(env_port);
+
+    Logger::info("Launching server...");
+    Server server(port);
     server.start();
     return 0;
 }
