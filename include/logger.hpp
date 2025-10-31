a30d0cbf9e82ba8f04f6f5b5c27c4fb406cec93e
bkandh30 <kandhari.bhavya20@gmail.com>
2025-10-30 22:50:10 -0700

    Phase 6: Created logger class

---
+++ include/logger.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <chrono>
+#include <ctime>
+
+enum class LogLevel { INFO, WARN, ERROR };
+
+class Logger {
+    public:
+        static void log(LogLevel level, const std::string& msg);
+        static void info(const std::string& msg) { log(LogLevel::INFO, msg); }
+        static void warn(const std::string& msg) { log(LogLevel::WARN, msg); }
+        static void error(const std::string& msg) { log(LogLevel::ERROR, msg); }
+}
