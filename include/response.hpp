7cc961a24f99f8bb8d5a3bc5d425d4ee4e8e6eab
bkandh30 <kandhari.bhavya20@gmail.com>
2025-10-30 20:45:08 -0700

    Phase 2: Create HTTP Response Contract

---
+++ include/response.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include <string>
+
+class Response {
+    public:
+        static std::string build(int status, const std::string& body, const std::string* contentType = "text/plain");
+}
