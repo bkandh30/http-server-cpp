4d21d8879f16fd5f786f47142f99910b18c32380
bkandh30 <kandhari.bhavya20@gmail.com>
2025-10-30 22:25:26 -0700

    Added router class

---
+++ include/router.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <functional>
+#include <string>
+#include <unordered_map>
+
+class Router {
+    public:
+        using Handler = std::function<std::string()>;
+
+        void get(const std::string& path, Handler handler);
+        bool has_route(const std::string& path) const;
+        std::string execute(const std::string& path) const;
+
+    private:
+        std::unordered_map<std::string, Handler> routes_;
+}
