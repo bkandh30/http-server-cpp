a32bc50cac828d359ec08796402f3c47ae1495d4
bkandh30 <kandhari.bhavya20@gmail.com>
2025-11-01 17:04:20 -0700

    Phase 9: Added metric aggregation

---
+++ src/metrics.cpp
@@ -0,0 +1,28 @@
+#include "metrics.hpp"
+#include <sstream>
+
+Metrics& Metrics::Instance() {
+    static Metrics instance;
+    return instance;
+}
+
+void Metrics::record_request(double latency_ms, bool cache_hit) {
+    std::lock_guard<std::mutex> lock(mutex_);
+    total_requests_++;
+
+    if (cache_hit) {
+        cache_hits_++;
+    }
+
+    avg_latency_ += (latency_ms - avg_latency_) / total_requests_.load();
+}
+
+std::string Metrics::json() const {
+    std::lock_guard<std::mutex> lock(mutex_);
+    std::ostringstream ss;
+    ss << "{"
+        << "\"total_requests\": " << total_requests_.load() << ","
+        << "\"cache_hits\": " << cache_hits_.load() << ","
+        << "\"avg_latency_ms\": " << avg_latency_ms_.load() << "}";
+    return ss.str();
+}
